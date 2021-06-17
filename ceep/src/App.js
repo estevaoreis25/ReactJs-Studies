a6e674262a1935e2c80f75bc1885420655240af4
estevaoreis25 <estevaoreis1587@gmail.com>
2021-06-17 11:01:09 -0300

    Creating components FormularioCadastro, ListaDeNotas, CardNota

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react'
+import React, {
+  Component
+} from 'react'
+import {
+  ListaDeNotas
+} from './components/ListaDeNotas.jsx'
+import FormularioCadastro from './components/FormularioCadastro.jsx'
 
-function App() {
-  return (
-    <>
-      <form>
-        <input type="text" placeholder="Título"/>
-        <textarea placeholder="Escreva sua nota..."/>
-        <button>Criar nota</button>
-      </form>
-      <ul>
-        <li>
-          <section>
-            <header>
-              <h3> Título </h3>
-            </header>
-            <p> Escreva sua nota</p>
-          </section>
-        </li>
-      </ul>
-    </>
-  );
+class App extends Component {
+  render() {
+    return ( 
+      <>
+      <FormularioCadastro / >
+      <ListaDeNotas / >
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ ceep/src/components/ListaDeNotas.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import CardNota from "./CardNota";
+
+class ListaDeNotas extends Component {
+  render(){
+    return(
+      <ul>
+        <li>
+          <CardNota/>
+        </li>
+      </ul >
+    )
+  }
+}
+export default ListaDeNotas;
