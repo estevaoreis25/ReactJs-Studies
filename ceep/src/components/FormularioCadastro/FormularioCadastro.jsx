1a3209139c734c4aee0620b45e04be7b8de80886
estevaoreis25 <estevaoreis1587@gmail.com>
2021-06-19 08:55:33 -0300

    Cleaning inputs after submit

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,50 +1,53 @@
 import React, { Component } from "react";
 import './style.css';
 
- class FormularioCadastro extends Component {
-   
-   constructor(props){
-     super(props);
-     this.titulo = "";
-     this.texto = "";
-   }
+class FormularioCadastro extends Component {
 
-   _handleMudancaTitulo(evento){
-      evento.stopPropagation();
-      this.titulo = evento.target.value;
-   }
-   _handleNota(evento){
+  constructor(props) {
+    super(props);
+    this.titulo = "";
+    this.texto = "";
+  }
+
+  _handleMudancaTitulo(evento) {
+    evento.stopPropagation();
+    this.titulo = evento.target.value;
+  }
+  _handleNota(evento) {
     evento.stopPropagation();
     this.texto = evento.target.value;
-   }
-   _criarNota(evento){
-     evento.preventDefault();
-     evento.stopPropagation();
-     this.props.criarNota(this.titulo, this.texto);
-   }
+  }
+  _criarNota(evento) {
+    evento.preventDefault();
+    evento.stopPropagation();
+    this.props.criarNota(this.titulo, this.texto);
+    console.log(evento.target[0]);
+    evento.target[0].value = "";
+    evento.target[1].value = "";
+  }
 
   render() {
     return (
-        <form className="form-cadastro"
-          onSubmit = {this._criarNota.bind(this)}
-        >
-          <input 
-            type="text" 
-            placeholder="Título" 
-            className="form-cadastro_input"
-            onChange = {this._handleMudancaTitulo.bind(this)}
-          />
-          <textarea 
-            rows = {15}
-            placeholder="Escreva sua nota..." 
-            className="form-cadastro_input"
-            onChange = {this._handleNota.bind(this)}
-            
-            />
-          <button className="form-cadastro_input form-cadastro_submit">
-            Criar nota
-          </button>
-        </form>
+      <form className="form-cadastro"
+        onSubmit={this._criarNota.bind(this)}
+      >
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+          onChange={this._handleMudancaTitulo.bind(this)}
+        />
+        <textarea
+          rows={15}
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+          onChange={this._handleNota.bind(this)}
+
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar nota
+        </button>
+      </form>
     );
   }
 }
